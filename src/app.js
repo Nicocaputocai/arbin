a950415c51224af0ab32e3779bc2726c90eb17ab
nicocaputocai <nicolas_caputo@hotmail.com>
2026-04-25 19:27:18 -0300

    corregí cors

--- src/app.js
+++ src/app.js
@@ -8,11 +8,13 @@ const Api = require('./routes/Api.js');
 const Trees = require('./routes/trees.js')
 const CensusTree = require("./routes/censusTree.js")
 const Users = require("./routes/users.js")
-const corsOptions ={
-    methods: 'GET, POST, PUT',
-    allowedHeaders: 'Content-Type,Authorization',
-    exposedHeaders: 'Content-Range,X-Content- Range'
-}
+const corsOptions = {
+    origin: '*', // Permite que tu localhost y la app móvil se conecten sin problemas
+    methods: 'GET, POST, PUT, DELETE, OPTIONS', // OPTIONS es vital para el chequeo de seguridad
+    allowedHeaders: 'Content-Type, Authorization, type, Accept, Origin, X-Requested-With', // Aquí agregamos 'type'
+    exposedHeaders: 'Content-Range, X-Content-Range'
+};
+
 App.use(cors(corsOptions));
 App.use(express.json({ limit: '50mb' }));
 App.use(express.urlencoded({ limit: '50mb', extended: true }));
